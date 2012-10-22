91848b68fb8bfc3fea93e13cc114e3ac293722c7
Andrew DeMaria <ademaria@mines.edu>
2012-10-22 15:47:03 -0600

    Actually Center.h was a different algorithm that we still might use so it is resurected but just a skeleton

--- Dynamic/Center.h
+++ Dynamic/Center.h
@@ -1,10 +1,17 @@
 /*
  * =====================================================================================
  *
- *       Filename:  TableMatch.h
+ *       Filename:  Center.h
  *
  *    Description:  This contains an alorithm to find the largest subsequence of
  *                  a string that is also a palindrome. 
+ *
+ *                  The idea is that every possible palindrome subsequence has a
+ *                  center.  The center is either a null character between two
+ *                  characters or an actual character.  For each of these
+ *                  centers find the maximum palindrome subsequence using that
+ *                  character as the center.  Then the largest max is the
+ *                  largest subsequence.  
  *
  *        Version:  1.0
  *        Created:  10/22/2012 10:55:33 AM
@@ -22,23 +29,10 @@
 #include <cstdlib>
 #include <string>
 
-class TableMatch {
+class Center {
     public:
         // ctor
-        TableMatch( const std::string& original ) : original(original) { }
-
-        // wraps up all the dirty work into one step
-        std::string findMax();
-
-        // generates an empty table except with the diagonal as 1's
-        void generateEmptyTable();
-
-        // populates the table with 1's at the intersections of matching
-        // characters.  Only uses the upper right half of the table
-        void findMatches();
-
-        // adds indice, value, parent pairs to the given container.
-        void processRow();
+        Center( const std::string& original ) : original(original) { }
 
     private:
         std::string original;
