8cfd814c7bbcb7936e862f4a8a3eeb6780d065e1
Andrew DeMaria <ademaria@mines.edu>
2012-11-22 14:11:36 -0700

    Initial skeleton

---
+++ Wars/driver.cpp
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  driver.cpp
+ *
+ *    Description:  Driver file for the final project
+ *
+ *        Version:  1.0
+ *        Created:  11/22/2012 01:58:04 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Andrew DeMaria (muff1nman), ademaria@mines.edu
+ *   Organization:  Colorado School of Mines
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "    ALGO WARS" << endl;
+    cout << "=================" << endl;
+}
