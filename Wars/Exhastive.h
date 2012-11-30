83ff1ebc59f1eeb1f2583d28ea6001a8caf908e6
Andrew DeMaria <ademaria@mines.edu>
2012-11-30 11:37:12 -0700

    Exhastive bugs being fixed

--- Wars/Exhastive.h
+++ Wars/Exhastive.h
@@ -19,45 +19,10 @@
 #include <cstdlib>
 #include "ConnMatrix.h"
 #include <algorithm>
-
-std::vector<int> exhastive( ConnMatrix connections ) {
-    std::vector<int> currentBest;
-    std::vector<int> current;
-    int currentBestCost = INT_MAX;
-
-    // get a first permuate
-    for ( int i = 0; i < connections.size(); ++i ) {
-        current.append( i );
-    }
-
-    std::sort( current.begin(), current.end() );
-
-    // for each permutation: 
-    //    see if it beats the current champ
-    int currentCost;
-    while ( std::next_permutation( current.begin(), current.end() ) ) {
-        currentCost = calculatePathCost( connections, current );
-        if ( currentCost < currentBestCost ) {
-            currentBest = current;
-        }
-
-    }
-
-}
-
-// calculates the cost of a given path using the supplied connection
-// matrix. The connection matrix is indexed base 0 so there are
-// components 0 to (n - 1)
-int calculatePathCost( ConnMatrix connections, std::vector<int> path ) {
-    int cost = 0;
-    // for each pair of componets, ( i and j) add the distance *
-    // number of connections between those components to the cost
-    for ( int i = 0; i < connections.size(); ++i ) {
-        for( int j = i + 1; j < connections[i].size(); ++j ) {
-            cost += distance( path, i, j ) * connections[i][j];
-        }
-    }
-}
+#include <vector>
+#include <climits>
+#include "debug.h"
+#include "Helper.h"
 
 // calculate the absolute distance between two unique componets in a
 // array.  (i.e. if they are adjacent, return 1.  If there is a
@@ -73,4 +38,50 @@ int distance( std::vector<int> path, int one, int two ) {
 
     return abs( one_index - two_index );
 }
+
+// calculates the cost of a given path using the supplied connection
+// matrix. The connection matrix is indexed base 0 so there are
+// components 0 to (n - 1)
+int calculatePathCost( ConnMatrix connections, std::vector<int> path ) {
+    int cost = 0;
+    // for each pair of componets, ( i and j) add the distance *
+    // number of connections between those components to the cost
+    for ( int i = 0; i < connections.size(); ++i ) {
+        for( int j = i + 1; j < connections[i].size(); ++j ) {
+            cost += distance( path, i, j ) * connections[i][j];
+        }
+    }
+
+    return cost;
+}
+
+
+std::vector<int> exhastive( ConnMatrix connections ) {
+    std::vector<int> currentBest;
+    std::vector<int> current;
+    int currentBestCost = INT_MAX;
+
+    // get a first permuate
+    for ( int i = 0; i < connections.size(); ++i ) {
+        current.push_back( i );
+    }
+
+    std::sort( current.begin(), current.end() );
+
+    // for each permutation: 
+    //    see if it beats the current champ
+    int currentCost;
+    while ( std::next_permutation( current.begin(), current.end() ) ) {
+        currentCost = calculatePathCost( connections, current );
+        if ( currentCost < currentBestCost ) {
+            currentBest = current;
+        }
+
+    }
+
+    return currentBest;
+
+}
+
+
 
