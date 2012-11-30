8fa27c118266d8312deb281306af4ebaedf363af
Aakash Shah <ashah@mines.edu>
2012-11-30 11:37:58 -0700

    Sorted edges from matrix

--- Wars/Matt.h
+++ Wars/Matt.h
@@ -16,6 +16,35 @@
  * =====================================================================================
  */
 
- class 
+
+class ModuleConnection {
+  public:
+    ModuleConnection(int v1, int v2, int w) :
+      vertex1(v1),
+      vertex2(v2),
+      weight(w) {}
+    
+    bool operator<(const ModuleConnection& other) { return weight < other.weight; }
+
+    int vertex1;
+    int vertex2;
+    int weight;
+    
+};
+std::vector<int> stech(ConnMatrix connections) {
+  vector<ModuleConnection> edges;
+
+  for (int i = 0; i < connections.size(); ++i) {
+    for (int j = i; j < connections.at(i).size(); ++i) {
+      ModuleConnection c(i, j, connections.at(i).at(j));
+      edges.push_back(c);
+    }
+  }
+
+  std::sort( edges.begin(), edges.end() );
+
+  for (int i = 0; i < edges.size(); ++i)
+    cout << "Edge " << i << ": " << edges.at(i) << endl;
+}
 
 
