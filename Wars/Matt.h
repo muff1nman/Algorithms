08c6cab424d167e4333c8ede97a15bbbf6b78f4d
Aakash Shah <ashah@mines.edu>
2012-11-30 11:25:22 -0700

    Matt.h going strong

---
+++ Wars/Matt.h
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Matt.h
+ *
+ *    Description:  A greedy algorithm made by Matthew Stech Ph.d
+ *
+ *        Version:  1.0
+ *        Created:  11/30/2012 11:23:00am MTC
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Matthew Stech (mstech@mines.edu)
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+ class 
+
+
